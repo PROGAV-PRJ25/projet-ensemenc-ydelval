9a1c05c19c11527f1de762d8515f99a4d962733b
Yannick <yannick@localhost.localdomain>
2025-04-14 15:55:47 +0200

    création classe météo

--- Projet_info_S2/Program.cs
+++ Projet_info_S2/Program.cs
@@ -1,1 +1,3 @@
 ﻿Console.WriteLine("Le jeu du potagerrr") ; 
+Meteo meteo = new Meteo("été");
+Console.WriteLine($"Température : {meteo.Temperature}°C, Soleil : {meteo.Ensoleillement}, Événement : {meteo.EvenementSpecial}");
